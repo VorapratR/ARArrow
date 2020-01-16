8a466dd73d3f6ef2741c10dfce80a9daa79ffb2e
Woraprat Rattana <worapratrattana@MillenniumFalcon-2.local>
2020-01-16 08:02:22 +0700

    Ar Arrow

---
+++ Assets/webCamScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class webCamScript : MonoBehaviour {
+
+  public GameObject webCameraPlane; 
+  public Button fireButton;
+
+  // Use this for initialization
+  void Start () {
+
+    // if (Application.isMobilePlatform) {
+    //   GameObject cameraParent = new GameObject ("camParent");
+    //   cameraParent.transform.position = this.transform.position;
+    //   this.transform.parent = cameraParent.transform;
+    //   cameraParent.transform.Rotate (Vector3.right, 90);
+    // }
+
+    // Input.gyro.enabled = true;
+
+    fireButton.onClick.AddListener (OnButtonDown);
+
+    WebCamTexture webCameraTexture = new WebCamTexture();
+    webCameraPlane.GetComponent<MeshRenderer>().material.mainTexture = webCameraTexture;
+    webCameraTexture.Play();
+
+  }
+
+
+  void OnButtonDown(){
+
+    Debug.Log("Hello: ");
+    GameObject bullet = Instantiate(Resources.Load("arrow", typeof(GameObject))) as GameObject;
+    Rigidbody rb = bullet.GetComponent<Rigidbody>();
+    bullet.transform.rotation = Camera.main.transform.rotation;
+    bullet.transform.position = Camera.main.transform.position;
+    rb.AddForce(Camera.main.transform.forward * 5500f);
+    Destroy (bullet, 1);
+
+  }
+  
+  // Update is called once per frame
+  void Update () {
+
+    Quaternion cameraRotation = new Quaternion (Input.gyro.attitude.x, Input.gyro.attitude.y, -Input.gyro.attitude.z, -Input.gyro.attitude.w);
+    this.transform.localRotation = cameraRotation;
+  
+  }
+}
